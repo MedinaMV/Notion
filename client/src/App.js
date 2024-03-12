2655eb60058f403a065c77f8722e42bd9ff6bede
MedinaMV <alejandro.medina6@alu.uclm.es>
2024-03-12 21:41:17 +0100

    Lateral menu created

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,59 @@
-import * as React from 'react';
+//import { useState } from 'react';
 import './App.css';
 
-function App() {
-  const [text, setText] = React.useState("Perras")
+export default function App() {
   return (
-    <> 
-      <div className='perras'>{text}</div>
-      <button onClick={() => {
-        setText("Hola")
-      }}></button>
-    </>
-    
+    <div className="app">
+      <LateralMenu />
+      <div className="note">
+        <Note />
+      </div>
+    </div>
   );
 }
 
-export default App;
+function LateralMenu() {
+  return (
+    <div className="lateral">
+      <h2> My Notes </h2>
+        <div>
+          <NoteRow />
+          <NoteRow />
+          <NoteRow />
+        </div>
+      <div className="noteButtonContainer">
+        <button className="noteButton"> ➕ New Note </button>
+      </div>
+    </div>
+  );
+}
+
+function NoteRow() {
+  return (
+    <div className="noteRow">
+      <p className="noteTitle">Title</p>
+      <div className="buttonsContainer">
+        <button> 🔗 </button>
+        <button> ❌ </button>
+      </div>
+    </div>
+  );
+}
+
+function Note() {
+  return (
+  <>
+    <h1>Title</h1>
+    <Paragraph />
+  </>
+  );
+}
+
+function Paragraph() {
+  return (
+    <div>
+      <input></input>
+      <button> 💾 Save </button>
+    </div>
+  );
+}
