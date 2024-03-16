10d820bfb60978b3cc170b42423785af05c06fad
MedinaMV <valderasalejandro53@gmail.com>
2024-03-16 15:28:25 +0100

    Added types to note

--- server/models/notes.js
+++ server/models/notes.js
@@ -3,14 +3,17 @@ import mongoose from 'mongoose';
 const noteSchema = new mongoose.Schema({
     title: { type: String, required: true },
     paragraphs: [{
+        type: { type: String, default: 'paragraph' },
         content: { type: String },
         creation_time: { type: Date, default: Date.now }
     }],
     images: [{
+        type: { type: String, default: 'image' },
         content: { type: String },
         creation_time: { type: Date, default: Date.now}
     }],
     lists: [{
+        type: { type: String, default: 'list' },
         items: [{ type: String }],
         creation_time: { type: Date, default: Date.now }
     }]
